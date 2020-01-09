dd5d21a5590c65463be40ee3b2d38993f936de50
ashwinbalajir <ashwin.achu.1311@gmail.com>
2020-01-09 22:15:45 +0530

    first commit

---
+++ Node-samples/node_redis/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+let app=express();
+
+const port=3000;
+
+
+const redis = require('redis');
+const client = redis.createClient({
+  host:'redis-server',
+  port:6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+ client.get('visits', (err, visits) => {
+   res.send('Number of visits is ' + visits);
+   client.set('visits', parseInt(visits) + 1);
+ });
+});
+
+
+app.listen(port,()=>{
+ console.log(`listening on port ${port}`);
+ 
+})
